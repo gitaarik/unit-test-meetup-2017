5d9799d715b9f44c514417c13963d8b48228916f
Marco Beelen <marco.beelen@ximedes.com>
2017-01-13 21:57:13 +0100

    Added a failing test to allow the participants to start by making a failing test pass

---
+++ test-javascript-with-jasmine/spec/FailingByDefault.js
@@ -0,0 +1,9 @@
+describe("Demonstrate failing Jasmine Spec", function () {
+
+    it("has been hardcoded to fail", function () {
+        var product = 2 * 5;
+        // TODO: Please correct the expectations
+        expect(product).toEqual(11);
+    })
+
+});
