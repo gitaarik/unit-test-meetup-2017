38402fab3cfef4e73c2ea9fc5efb90791cb6b6c1
Rik <gitaarik@gmail.com>
2017-02-16 22:14:30 +0100

    added failing test

--- javascript/testing-with-jasmine/spec/GameOfLifeSpec.js
+++ javascript/testing-with-jasmine/spec/GameOfLifeSpec.js
@@ -17,7 +17,7 @@ describe("GameOfLife", function () {
         expect(game.isAlive([0, 0])).toBeFalsy()
     })
 
-    it("stays alive when 0,0 has 2 or 3 neighbours", function () {
+    it("stays alive when 0,0 has 2 neighbours", function () {
         game.setAlive([0, 0], true)
         game.setAlive([0, 1], true)
         game.setAlive([1, 0], true)
@@ -25,12 +25,40 @@ describe("GameOfLife", function () {
         expect(game.isAlive([0, 0])).toBeTruthy()
     })
 
-    it("stays alive when 5,5 has 2 or 3 neighbours", function () {
+    it("stays alive when 0,0 has 3 neighbours", function () {
+        game.setAlive([0, 0], true)
+        game.setAlive([1, 0], true)
+        game.setAlive([0, 1], true)
+        game.setAlive([1, 1], true)
+        game.nextFrame()
+        expect(game.isAlive([0, 0])).toBeTruthy()
+    })
+
+    it("stays alive when 5,5 has 2 neighbours", function () {
         game.setAlive([5, 5], true)
         game.setAlive([5, 6], true)
         game.setAlive([6, 5], true)
         game.nextFrame()
         expect(game.isAlive([5, 5])).toBeTruthy()
+    })
+
+    it("stays alive when 5,5 has 3 neighbours", function () {
+        game.setAlive([5, 5], true)
+        game.setAlive([6, 5], true)
+        game.setAlive([5, 6], true)
+        game.setAlive([6, 6], true)
+        game.nextFrame()
+        expect(game.isAlive([5, 5])).toBeTruthy()
+    })
+
+    it("dies when 5,5 has 4 neighbours", function () {
+        game.setAlive([5, 5], true)
+        game.setAlive([5, 6], true)
+        game.setAlive([6, 5], true)
+        game.setAlive([6, 6], true)
+        game.setAlive([4, 5], true)
+        game.nextFrame()
+        expect(game.isAlive([5, 5])).toBeFalsy()
     })
 
 });
