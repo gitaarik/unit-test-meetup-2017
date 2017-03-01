0b094c730c837ef76bf0537cff2a9285d07b7a70
Marco Beelen <marco.beelen@ximedes.com>
2017-03-01 23:50:18 +0100

    Prepared Primefactors Kata

---
+++ javascript/testing-with-jasmine/spec/PrimefactorsSpec.js
@@ -0,0 +1,15 @@
+describe("Primefactors", function () {
+
+    var primefactors;
+
+    beforeEach(function () {
+        primefactors = new Primefactors();
+    });
+
+
+    // it("should determine that one has not prime factors", function () {
+    //     expect(primefactors.find(1)).toEqual([]);
+    // });
+
+
+});
